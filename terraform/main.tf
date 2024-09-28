ea2ceeb457dc4951f0a05991c79fb1186321b3ba
choigunwoo <gunwoo8873@outlook.kr>
2024-09-28 23:07:56 +0900

    Update : Variable Port, Group, Name

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = "ap-northeast-3"
 }
@@ -15,27 +14,27 @@ provider "aws" {
 resource "aws_security_group" "instance" {
   name = "simple-web-sg"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.web_port
+    to_port = var.web_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "test_terraform" {
+resource "aws_instance" "simple" {
   ami         = "ami-0a07ff89aacad043e" # Instance OS image ami ID
   instance_type = "t3.micro"
-#   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+#   vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
-    name = "test_terraform"
+    Name = "test_terraform"
   }
 
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, Terraform!!" > index.html
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p "${var.web_port}" &
     EOF
 }
 
