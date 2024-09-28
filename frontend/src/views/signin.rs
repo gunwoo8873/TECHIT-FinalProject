fc8341e3a1c12bc0bd0ce9d282cefa26b0dc5259
choigunwoo <gunwoo8873@outlook.kr>
2024-09-28 16:36:52 +0900

    Yew and Web App : Signin

---
+++ frontend/src/views/signin.rs
@@ -0,0 +1,31 @@
+use yew::{function_component, html, Html};
+
+#[function_component(Signin)]
+pub fn signin() -> Html {
+    html! {
+        <>
+        <header>
+            <form action="/api/{id}" method="get">
+                <div>
+                    <a id="1" class="header__menu">{"Test"}</a>
+                    <a id="2" class="header__menu">{"Test"}</a>
+                    <a id="3" class="header__menu">{"Test"}</a>
+                    <a id="4" class="header__menu">{"Test"}</a>
+                    <a id="5" class="header__menu">{"Test"}</a>
+                </div>
+            </form>
+        </header>
+        <main>
+            <section>
+                <form action="/index/{id}" method="post">
+                    <div>
+                    </div>
+                </form>
+            </section>
+        </main>
+        <footer>
+            <h1>{"TEST"}</h1>
+        </footer>
+        </>
+    }
+}
