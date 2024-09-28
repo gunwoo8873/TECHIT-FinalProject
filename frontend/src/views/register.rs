904e97b729c52b1d9a1aa562295d5b444c97deb8
choigunwoo <gunwoo8873@outlook.kr>
2024-09-28 16:36:45 +0900

    Yew and Web App : Register

---
+++ frontend/src/views/register.rs
@@ -0,0 +1,13 @@
+use yew::{function_component, html, Html};
+
+#[function_component(Register)]
+pub fn register() -> Html {
+    html! {
+        <>
+        <main>
+            <section>
+            </section>
+        </main>
+        </>
+    }
+}
