83fbf1968fbcfbed3c9b2df3517e73abf2f8848a
choigunwoo <gunwoo8873@outlook.kr>
2024-09-29 19:34:15 +0900

    Update : Create Register, Signin API

--- frontend/src/api/types.rs
+++ frontend/src/api/types.rs
@@ -1,16 +1,28 @@
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct User {
     pub id: u64,
     pub name: String,
     pub email: String,
     pub password: String,
-    pub created_at: chrono<Utc>,
-    pub updated_at: chrono<Utc>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
 
-pub struct AppState {
-    pub users: Option<User>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserSignin {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserRegister {
+    pub id: u64,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+    pub address: String,
+    pub created_at: DateTime<Utc>,
 }
