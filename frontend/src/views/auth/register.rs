383eb19f19f5b5e83aea047717a0bb4cab53993d
choigunwoo <gunwoo8873@outlook.kr>
2024-09-29 19:32:14 +0900

    Fix : Header Remove and Not Update

--- frontend/src/views/auth/register.rs
+++ frontend/src/views/auth/register.rs
@@ -6,26 +6,16 @@ use crate::router::Route;
 pub fn register() -> Html {
     html! {
         <>
-        <header>
-            <div class="header__menu">
-                <Link<Route> to={Route::Index}>{ "Home" }</Link<Route>>
-                <Link<Route> to={Route::AMM}>{ "자산관리" }</Link<Route>>
-            </div>
-            <div>
-                //// Dynamic User Signin & Register Feature
-                <Link<Route> to={Route::Signin}>{ "signin" }</Link<Route>>
-                <Link<Route> to={Route::Register}>{ "register" }</Link<Route>>
-            </div>
-        </header>
         <main>
         <section>
         <div>
-            <h1>{"Register Page"}</h1>
             <form>
-                <label for="username">{"Username:"}</label>
-                <input type="text" id="username" name="username" />
-                <label for="password">{"Password:"}</label>
-                <input type="password" id="password" name="password" />
+                <label for="user_email">{"Email"}</label>
+                <input type="email" id="user_email" name="useremail"/>
+                <label for="user_ps">{"Password"}</label>
+                <input type="password" id="user_ps" name="password"/>
+                <label for="user_address">{"Address"}</label>
+                <input type="text" id="user_address" name="address"/>
                 <button type="submit">{"Register"}</button>
             </form>
         </div>
