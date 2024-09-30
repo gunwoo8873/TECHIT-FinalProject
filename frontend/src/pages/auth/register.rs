c840c65defc764106d01d82fcc682ab686121188
choigunwoo <gunwoo8873@outlook.kr>
2024-09-30 17:46:11 +0900

    Delete : HTML Directory Rename views -> pages

---
+++ frontend/src/pages/auth/register.rs
@@ -0,0 +1,38 @@
+use yew::{function_component, html, Html};
+// use yew_router::prelude::*;
+// use crate::router::Route;
+
+#[function_component(Register)]
+pub fn register() -> Html {
+    html! {
+        <>
+        <main>
+        <section>
+        <div>
+            <form>
+                <label for="user_id">{"Userid"}</label>
+                <input type="text" id="user_id" class="register_input" name="userid"/>
+
+                <label for="user_email">{"Email"}</label>
+                <input type="email" id="user_email" class="register_input" name="useremail"/>
+
+                <label for="user_ps">{"Password"}</label>
+                <input type="password" id="user_ps" class="register_input" name="password"/>
+
+                <label for="user_ps_check">{"Password Check"}</label>
+                <input type="password" id="user_ps_check" class="register_input" name="password_check"/>
+
+                <label for="user_phone">{"Phone Number"}</label>
+                <input type="text" id="user_phone" class="register_input" name="phone"/>
+
+                <label for="user_address">{"Address"}</label>
+                <input type="text" id="user_address" class="register_input" name="address"/>
+
+                <button type="submit" id="register_btn">{"Register"}</button>
+            </form>
+        </div>
+        </section>
+        </main>
+        </>
+    }
+}
