ca12866469c9bf782d65c482ac1554492c45924d
choigunwoo <gunwoo8873@outlook.kr>
2024-10-01 11:31:37 +0900

    Update : .env to Variable and Lib add

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,18 +1,21 @@
 use actix_web::{web, App, HttpServer};
-use std::io::Result;
+use std::{io::Result, env};
 
 pub async fn run_server() -> Result<()> {
-    let port = 5000;
-    let host = "127.0.0.1";
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "5000".to_string())
+        .parse()
+        .expect("PORT must be a number");
 
-    println!("Run server : http://127.0.0.1:5000");
+    println!("Run server : http://{host}:{port}");
 
     HttpServer::new(|| {
         App::new()
-            .service(web::scope("/api")
+            .service(web::scope("/")
             )
     })
-        .bind((host, port))?
+        .bind((host.as_str(), port))?
         .run()
         .await
 }
