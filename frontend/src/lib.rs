e8f24c28dd4d4577197e0fbfdbd404f6dea7c056
choigunwoo <gunwoo8873@outlook.kr>
2024-10-08 16:27:45 +0900

    Fix : Delete Struct

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,16 +1,2 @@
-use serde::{Deserialize, Serialize};
-
 pub mod views;
 pub mod route;
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct UserConfig {
-    pub id: i32,
-    pub user_name: String,
-    pub user_id: String,
-    pub user_email: String,
-    pub user_password: String,
-    pub password_check: String,
-    pub user_phone: String,
-    pub token: String,
-}
