e1a7053acde75188bdad460e9c568e6cb91bbb83
choigunwoo <gunwoo8873@outlook.kr>
2024-10-10 17:02:41 +0900

    Fix : K8s Client, Resource Code Refactoring

--- kubernetes/src/api.rs
+++ kubernetes/src/api.rs
@@ -1,59 +1,36 @@
-use kube::{
-    Api,
-    Client,
-    api::{ListParams},
-};
 use k8s_openapi::api::core::v1::{
-    Pod, Service, ConfigMap,
+    Pod, Service, ConfigMap, PodSpec, PodStatus, Container,
 };
-use std::{
-    error::Error,
+use kube::{
+    Config,
+    Client,
+    Api,
+    config::KubeConfigOptions,
+    api::{ListParams, DeleteParams, Resource},
 };
+use std::error::Error;
 
-async fn create_clinet() -> Result<Client, Box<dyn Error>> {
-    let config = kube::Config::from_kubeconfig(
-        &kube::config::KubeConfigOptions::default()
+#[tokio::main]
+async fn kube_client() -> Result<(), Box<dyn Error>> {
+    let config = Config::from_kubeconfig(
+        &KubeConfigOptions::default()
     ).await?;
-
     let client = Client::try_from(config)?;
 
-    Ok(client)
-}
-#[tokio::main]
-pub async fn get_pods() -> Result<(), Box<dyn Error>> {
-    let client = create_clinet().await?;
-    let lp = ListParams::default();
-
-    let pods: Api<Pod> = Api::default_namespaced(client);
-    for p in pods.list(&lp).await? {
-        println!("Found Pod: {}", p.metadata.name.unwrap_or_default());
-    }
-
     Ok(())
 }
 
-#[tokio::main]
-pub async fn get_services() -> Result<(), Box<dyn Error>> {
-    let client = create_clinet().await?;
-    let lp = ListParams::default();
-
-    let services: Api<Service> = Api::default_namespaced(client);
-    for s in services.list(&lp).await? {
-        println!("Found Service: {}", s.metadata.name.unwrap_or_default());
-    }
-
-    Ok(())
+enum Resources {
+    Pod(Pod),
+    PodSpec(PodSpec),
+    PodStatus(PodStatus),
+    Service(Service),
 }
 
-#[tokio::main]
-pub async fn get_configmaps() -> Result<(), Box<dyn Error>> {
-    let client = create_clinet().await?;
-    let lp = ListParams::default();
-
-    let configmaps: Api<ConfigMap> = Api::default_namespaced(client);
-    for c in configmaps.list(&lp).await? {
-        println!("Found ConfigMap: {}", c.metadata.name.unwrap_or_default());
-    }
-
-    Ok(())
-}
+// #[tokio::main]
+// pub async fn get_resource() -> Result<Vec<Resources>, Box<dyn Error>> {
+//     let client = kube_client().await?;
+//     let lp = ListParams::default();
+//
+//     Ok()
+// }
